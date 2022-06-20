f67723baa54cf1778838ccae923ca52eb35e8b02
kagithapk <kagithapk@gmail.com>
2022-06-20 23:14:03 +0530

    initial commit

---
+++ pages/api/test/[id].ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+	name: string;
+};
+
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+	const { id } = req.query;
+	res.status(200).json({ name: `From pages/api/test/[id].tsJohn Doe ${id}` });
+}
